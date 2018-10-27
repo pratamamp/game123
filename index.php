f1c21d7dd19948866ab9ba844983fe7643d437d3
pratamamp <pratamamp@gmail.com>
2018-10-27 13:52:00 +0700

    add files

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+<head>
+    <style type = "text/css">
+        body{
+            background: #000000;
+            padding: 0px;
+            margin: 0px;
+        }
+        canvas{
+            display:block;
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
+    <script src = "phaser.min.js"></script>
+    <script src = "game.js"></script>
+</head>
+<body>
+</body>
+</html>
